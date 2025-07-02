8b133d367828549a51f8fa6157dcd8d6b7193683
ribeiro-dev <romilson.2r@gmail.com>
2025-07-01 21:46:19 -0300

    fix: allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["https://realtime-chat-front.onrender.com/"],
+    allowedHosts: ["realtime-chat-front.onrender.com"],
   },
 });
