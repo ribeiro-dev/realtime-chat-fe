9c1368cc03037b0e9a3f44e0b5045af96b4444cf
ribeiro-dev <romilson.2r@gmail.com>
2025-07-21 22:09:59 -0300

    ref: change logic to use a socket service class

--- src/pages/Chat/components/Body/index.tsx
+++ src/pages/Chat/components/Body/index.tsx
@@ -1,118 +1,57 @@
-import { FormEvent, useEffect, useRef, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useRef } from "react";
+import { Socket } from "socket.io-client";
 
 import { Message } from "./components/Message";
 import { MessageForm } from "./components/MessageForm";
 import { Loading } from "./components/Loading";
 
 import { Container, MessagesContainer, MessagesWrapper } from "./styles";
-import { socket } from "../../../../socket";
 
-interface Message {
-  id: string
-  user: string
-  content: string
-  date: Date
+interface BodyProps {
+  messages: Message[];
+  socket: Socket | null;
+  handleFormSubmit: (e: React.FormEvent) => void;
 }
 
-export function Body() {
-  const [messages, setMessages] = useState<Message[]>([]);
-  const [username, setUsername] = useState<string>("");
+interface Message {
+  id: string;
+  user: string;
+  content: string;
+  date: Date;
+}
 
-  const messagesEndRef = useRef<HTMLDivElement | null>(null)
-  const navigate = useNavigate()
-
-  function sendMessage(e: FormEvent) {
-    e.preventDefault();
-    const form = e.target as HTMLFormElement;
-    const value = (form[0] as HTMLInputElement).value.trim(); // handles the .value error
-
-    const messageDate = new Date()
-    const messageId = socket.id! + messageDate.getTime()
-
-    const newMessage: Message = {
-      id: messageId,
-      user: username,
-      content: value,
-      date: messageDate,
-    }
-    socket.emit('messages@new', newMessage)
-
-    setMessages(prevState => {
-      return [...prevState, newMessage]
-    })
-  }
+export function Body({ messages, socket, handleFormSubmit }: BodyProps) {
+  const messagesEndRef = useRef<HTMLDivElement | null>(null);
+  const username = localStorage.getItem("username");
 
   useEffect(() => {
-    const user = localStorage.getItem("username")
-    if (!user) {
-      navigate("/")
-      return
-    }
-
-    setUsername(user)
-  }, [navigate])
-
-  // Socket Listeners
-  useEffect(() => {
-    const getMessages = (data: Message[]) => {
-      const formattedData = data.map((message: Message) => {
-        message.date = new Date(message.date)
-
-        return message
-      })
-      setMessages(formattedData);
-
-      console.log(data)
-    }
-
-    const getNewMessage = (data: Message) => {
-      setMessages(prevState => {
-        data.date = new Date(data.date)
-        return [...prevState, data]
-      })
-    }
-
-    socket.on("getMessages", getMessages)
-    socket.on('messages@new', getNewMessage)
-
-    return () => {
-      socket.off("getMessages", getMessages)
-      socket.off("messages@new", getNewMessage)
-    }
-  }, [])
-
-  useEffect(() => {
-    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' })
-  }, [messages])
-
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
 
   return (
     <Container>
-      {!socket.connected
-        ? (
-          <Loading />
-        ) : (
-          <MessagesContainer>
-            <MessagesWrapper>
-              {messages.map(({ id, user, content, date }) => (
-                <Message
-                  key={id}
-                  user={user}
-                  content={content}
-                  date={date.toLocaleString()}
-                  owner={user == username}
-                />
-              ))}
+      {!socket?.connected ? (
+        <Loading />
+      ) : (
+        <MessagesContainer>
+          <MessagesWrapper>
+            {messages.map(({ id, user, content, date }) => (
+              <Message
+                key={id}
+                user={user}
+                content={content}
+                date={date.toLocaleString()}
+                owner={user == username}
+              />
+            ))}
 
-              {/* Only to scroll reference */}
-              <div ref={messagesEndRef} />
-              </MessagesWrapper>
-          </MessagesContainer>
-        )
-      }
+            {/* Only to scroll reference */}
+            <div ref={messagesEndRef} />
+          </MessagesWrapper>
+        </MessagesContainer>
+      )}
 
-      <MessageForm onSubmit={sendMessage} />
+      <MessageForm onSubmit={handleFormSubmit} />
     </Container>
   );
 }
